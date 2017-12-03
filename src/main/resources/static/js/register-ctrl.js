38049e6947149a193589def546902f29687660aa
rpytel1 <rafal.pytel1@wp.pl>
2017-12-03 22:01:10 +0100

    basic logging and registering with redirect

--- src/main/resources/static/js/register-ctrl.js
+++ src/main/resources/static/js/register-ctrl.js
@@ -1,6 +1,37 @@
 angular.module('app', [])
-    .controller('registerCtrl', function ($scope, $http) {
+    .controller('registerCtrl', function ($scope, $http, $location) {
+        $scope.register = register;
+        $scope.loginButton = loginButton;
 
+        var url=$location.absUrl().replace("views/register.html","index.html");
+
+        function register() {
+            console.log($location.absUrl());
+            $http.post("http://localhost:8080/register", {
+                'firstname': $scope.firstName,
+                'lastname': $scope.lastName,
+                'email': $scope.email,
+                'password': $scope.passwordRegister
+            })
+                .success(function () {
+                    console.log("Register!!!");
+                     window.location.assign( url);
+                });
+        }
+
+        function loginButton() {
+            window.location.assign("./cart.html");
+            window.location.assign( url);
+            $http.post("http://localhost:8080/login", {
+                'login': $scope.login,
+                'pasword': $scope.passwordLogin
+
+            })
+                .success(function () {
+                    console.log("Logged!!!");
+                    window.location.assign( url);
+                });
+        }
     });
 
 
