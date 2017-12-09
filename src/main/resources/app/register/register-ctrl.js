ecc75307927eb77b4f70ffa9b151622027c0c5f9
rpytel1 <rafal.pytel1@wp.pl>
2017-12-09 22:04:45 +0100

    working login and register

--- src/main/resources/app/register/register-ctrl.js
+++ src/main/resources/app/register/register-ctrl.js
@@ -9,8 +9,14 @@ angular.module('myApp.register', [])
     .controller('RegisterCtrl', function ($scope, $http, $location) {
         $scope.register = register;
         $scope.loginButton = loginButton;
+        $scope.chooseRegister = chooseRegister;
+        $scope.chooseLogin = chooseLogin;
+        $scope.loginPanel = false;
+        $scope.registerPanel = true;
+        $scope.signup = "active";
 
-        var url = $location.absUrl().replace("register", "index");
+
+        // var url = $location.absUrl().replace("register", "index");
 
         function register() {
             console.log($location.absUrl());
@@ -22,11 +28,11 @@ angular.module('myApp.register', [])
             })
                 .success(function () {
                     console.log("Register!!!");
-                    window.location.assign(url);
                 });
         }
 
         function loginButton() {
+
             window.location.assign("/cart");
             window.location.assign(url);
             $http.post("http://localhost:8080/login", {
@@ -36,8 +42,26 @@ angular.module('myApp.register', [])
             })
                 .success(function () {
                     console.log("Logged!!!");
-                    window.location.assign(url);
+                    window.location.assign("/cart");
+
                 });
+        }
+
+        function chooseLogin() {
+            $scope.loginPanel = true;
+            $scope.registerPanel = false;
+            console.log($scope.loginPanel);
+            $scope.signup = "";
+            $scope.loginClass = "active";
+
+        }
+
+        function chooseRegister() {
+            $scope.loginPanel = false;
+            $scope.registerPanel = true;
+            $scope.signup = "active";
+            $scope.loginClass = "";
+            console.log("Reg" + $scope.registerPanel);
         }
     });
 
